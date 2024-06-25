32ac40d2b465793978a068608b74efbeef7bf02c
Foggy <hrimar321@gmail.com>
2024-06-25 17:34:10 +0100

    submit test

---
+++ submit.js
@@ -0,0 +1,46 @@
+var selected_color= '#ed2450'
+
+function pressedcolor(the_color,the_color_name){
+    selectedPointer = document.getElementById("selectedcolor")
+    selected_color = the_color
+    console.log("We have selected the color: " + the_color_name)
+    selectedPointer.innerHTML = "Selected color is " + the_color_name
+}
+
+async function submitAll(){
+    const nameElement = document.getElementById("uname")
+    const username = nameElement.value
+    var nameallowed = checkName(username)
+    if (nameallowed){
+        await make_post_request(username,selected_color)
+        alert("Succesfully submitted!")
+    }
+}
+
+function checkName(the_name){
+    { 
+        var letters = /^[A-Za-z]+$/;
+        if(the_name.match(letters))
+        {
+        return true;
+        }
+        else
+        {
+        alert('Please input alphabet characters only');
+        return false;
+        }
+    }
+}
+
+async function make_post_request(username,le_color){
+    await fetch("https://mypythonworker.hrimar321.workers.dev", {
+        method: "POST",
+        body: JSON.stringify({
+          name: username,
+          color: le_color,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      });
+}
