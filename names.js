8f38f2c8f8124f7f4306ae918b534118210b22c1
Foggy <hrimar321@gmail.com>
2024-06-25 18:32:25 +0100

    actual fix

--- names.js
+++ names.js
@@ -15,13 +15,17 @@ async function fetchData(){
 
 function writenames(names){
     const container = document.getElementById("namecontainer");
-    var names = []
+    var unique_names = []
     var the_id = 0
     var plus = true
     for (user in names){
         const userdata  = names[user]
         const name = userdata["name"]
         const color = userdata["color"]
+
+        if (unique_names.includes(name)){
+            return
+        }
 
         const mything = document.createElement("p")
         mything.style.color = color
@@ -34,14 +38,11 @@ function writenames(names){
         const node = document.createTextNode(name);
         mything.appendChild(node)
         setProperty(mything,"--charindex",the_id)
-        
-        if (names.includes(name)){
-            mything.remove()
-        }
+
 
         container.appendChild(mything)
         the_id += 1
-        names.push(name)
+        unique_names.push(name)
         plus = !plus
 
     }
