9eb1fb9044c9544b497900ff71d46ede00d24440
Foggy <hrimar321@gmail.com>
2024-06-25 14:01:41 +0100

    testing now

--- names.js
+++ names.js
@@ -2,7 +2,10 @@
 
 async function fetchData(){
     const IdThing = document.getElementById("loading");
+    const ResultsThing = document.getElementById("results");
+
     console.log(IdThing.con)
     const response = await fetch("https://mypythonworker.hrimar321.workers.dev");
     IdThing.remove();
+    ResultsThing.textContent = response
 }
