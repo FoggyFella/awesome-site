975f3647793558a27aa35aab2d0d4afccc6bbdc2
Foggy <hrimar321@gmail.com>
2024-08-22 13:29:34 +0100

    birthday test

---
+++ birthday.js
@@ -0,0 +1,26 @@
+async function submitAll(){
+    const nameElement = document.getElementById("uname")
+    const msgElement = document.getElementById("umsg")
+
+    const username = nameElement.value
+    const message = msgElement.value
+
+    if (nameallowed){
+        await make_post_request(username,message)
+        alert("Succesfully submitted!")
+        window.location.replace("https://foggydude.dev/names");
+    }
+}
+
+async function make_post_request(username,le_message){
+    await fetch("https://birthday-thing.hrimar321.workers.dev", {
+        method: "POST",
+        body: JSON.stringify({
+          name: username,
+          message: le_message,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      });
+}
