9ff89dad46015c1e7d0bc74ade270f6a3ec1f92a
Foggy <hrimar321@gmail.com>
2025-11-14 23:12:37 +0000

    testing time saving

--- FBAR/main.js
+++ FBAR/main.js
@@ -1,1 +1,12 @@
+function whenLoaded(){
+    var links = document.querySelectorAll('a');
 
+    links.forEach(element => {
+        element.addEventListener("click",leaving)
+    });
+}
+
+function leaving(){
+    var audio = document.querySelector("audio");
+    document.cookie = "musictime="+audio.currentTime.toString();
+}
