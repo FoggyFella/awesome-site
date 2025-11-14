2d0488ebfa5c472ea1ed39ac096659e2c325dd20
Foggy <hrimar321@gmail.com>
2025-11-14 23:18:00 +0000

    test2

--- FBAR/main.js
+++ FBAR/main.js
@@ -4,9 +4,28 @@ function whenLoaded(){
     links.forEach(element => {
         element.addEventListener("click",leaving)
     });
+
+    var audio = document.querySelector("audio");
+    audio.currentTime = parseInt(getCookie("musictime"));
 }
 
 function leaving(){
     var audio = document.querySelector("audio");
     document.cookie = "musictime="+audio.currentTime.toString();
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
