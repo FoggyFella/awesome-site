9fb1f3d7b5bed7f6da367ea0ba99bfb34f041192
Foggy <hrimar321@gmail.com>
2025-11-15 16:15:13 +0000

    radio sorta working

--- FBAR/main.js
+++ FBAR/main.js
@@ -5,14 +5,55 @@ function whenLoaded(){
         element.addEventListener("click",leaving)
     });
 
-    var audio = document.querySelector("audio");
-    audio.currentTime = parseInt(getCookie("musictime"));
+    var audio = document.getElementById("musicPlayer");
+    var theMusic = getCookie("musicName");
+    if (theMusic == "null"){
+      theMusic = getRandomSong();
+      document.cookie = "musicName="+theMusic;
+    }
+
+    audio.addEventListener("ended",onMusicEnded);
+    audio.src = "assets/music/"+theMusic+".mp3";
+    audio.load();
     audio.volume = 0.2;
+    audio.play();
+
+    var newTime = getCookie("musictime");
+    if (newTime != "null"){
+      audio.currentTime = parseInt(newTime);
+    }
+    getRandomSong();
+}
+
+function enableSuperSpeed(){
+  var audio = document.getElementById("musicPlayer");
+  audio.playbackRate=10.0;
+}
+
+function onMusicEnded(){
+    var theMusic = getRandomSong();
+    document.cookie = "musicName="+theMusic;
+    var audio = document.getElementById("musicPlayer");
+    audio.src = "assets/music/"+theMusic+".mp3";
+    audio.load();
+    audio.volume = 0.2;
+    audio.play();
 }
 
 function leaving(){
-    var audio = document.querySelector("audio");
+    var audio = document.getElementById("musicPlayer");
     document.cookie = "musictime="+(audio.currentTime+0.25).toString();
+}
+
+function getRandomSong(){
+  const songs = [
+    "Atoms",
+    "indream",
+    "rip_and_tear"
+  ]
+
+  var random_ind = Math.round(Math.random()*(songs.length-1));
+  return songs[random_ind];
 }
 
 function getCookie(cname) {
@@ -28,5 +69,5 @@ function getCookie(cname) {
       return c.substring(name.length, c.length);
     }
   }
-  return "";
+  return "null";
 }
