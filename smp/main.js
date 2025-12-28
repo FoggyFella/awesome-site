399fdbeb9680cad2afb8a8ee589de54ec73a153d
Foggy <hrimar321@gmail.com>
2025-12-28 18:54:43 +0000

    minecraft test

---
+++ smp/main.js
@@ -0,0 +1,24 @@
+async function checkPassword(){
+    const thePassword = String(document.getElementById("passwordinput").value);
+    const thePart = "1";
+
+    let thejson
+
+    await fetch("https://mypythonworker.hrimar321.workers.dev", {
+        method: "POST",
+        body: JSON.stringify({
+          thething: "smpcheck",
+          password: thePassword,
+          part: thePart
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      }).then((response) => response.json())
+    .then((json) => thejson=json);
+
+    if (thejson[0] == true){
+        const imgContainer = document.querySelector(".bigimage")
+        imgContainer.setAttribute("src",thejson[1]+".png")
+    }
+}
