6d738091b7a05776d58bd2ab189631ff23859e04
Foggy <hrimar321@gmail.com>
2026-06-28 14:36:01 +0100

    timestamps

--- FBAR/messagelog.js
+++ FBAR/messagelog.js
@@ -14,6 +14,7 @@ const defaultTest = [
 ]
 
 async function getData(theindex = 0) {
+    console.log(timeConverter(1782653285));
     const response = await fetch("https://general-messages.hrimar321.workers.dev",{
         method: "POST",
         body: JSON.stringify({thing:"GETMESSAGES",index: theindex})
@@ -32,10 +33,16 @@ function writeMessages(messages){
 
     const container = document.getElementById("messageContainer");
 
-    const newElements = messages.map((i)=>'<p class=username>'+i.username+':</p><p class=message>'+i.message+'</p><p class=channelName>in '+i.channelname+'</p>');
+    const newElements = messages.map((i)=>'<p class=username>'+i.username+':</p><p class=message>'+i.message+'</p><p class=channelName>in '+i.channelname+' at '+timeConverter(i.timestamp)+'</p>');
     container.insertAdjacentHTML("beforeend",newElements.join(''))
 }
 
 function update(){
     getData(currentlyLoaded)
+}
+
+
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  return a.toLocaleDateString()+" "+a.toLocaleTimeString();
 }
