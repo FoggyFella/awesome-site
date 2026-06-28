b3d9e1a8fad5f638ae0fc09d2dfc39386395979d
Foggy <hrimar321@gmail.com>
2026-06-28 12:53:17 +0100

    okbro

--- FBAR/messagelog.js
+++ FBAR/messagelog.js
@@ -1,7 +1,7 @@
 async function getData() {
     const response = await fetch("https://general-messages.hrimar321.workers.dev",{
         method: "POST",
-        body: JSON.stringify({thing:"GETMESSAGES",index: 1})
+        body: JSON.stringify({thing:"GETMESSAGES",index: 0})
     });
     console.log(await response.json());
 }
