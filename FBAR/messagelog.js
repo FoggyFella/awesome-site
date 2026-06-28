e5659d4b0587b8a4e660cfd2d13b2abd8c9fa832
Foggy <hrimar321@gmail.com>
2026-06-28 13:32:16 +0100

    time for a real test

--- FBAR/messagelog.js
+++ FBAR/messagelog.js
@@ -1,5 +1,18 @@
 currentlyLoaded = 0;
 
+const defaultTest = [
+    {
+        "username": "fuvk",
+        "message": "hello guys",
+        "channelname": "leaks"
+    },
+    {
+        "username": "foggy",
+        "message": "whats up you",
+        "channelname": "general"
+    }
+]
+
 async function getData(theindex = 0) {
     const response = await fetch("https://general-messages.hrimar321.workers.dev",{
         method: "POST",
@@ -7,7 +20,20 @@ async function getData(theindex = 0) {
     });
     const jsonResponse = await response.json();
 
-    currentlyLoaded = jsonResponse.length;
     console.log(jsonResponse);
-    console.log(currentlyLoaded)
+    console.log(currentlyLoaded);
+    writeMessages(jsonResponse);
+}
+
+function writeMessages(messages){
+    //messages = defaultTest;
+
+    const container = document.getElementById("messageContainer");
+
+    const newElements = messages.map((i)=>'<p class=username>'+i.username+':</p><p class=message>'+i.message+'</p><p class=channelName>in '+i.channelname+'</p>');
+    container.insertAdjacentHTML("beforeend",newElements.join(''))
+}
+
+function update(){
+    getData(currentlyLoaded)
 }
